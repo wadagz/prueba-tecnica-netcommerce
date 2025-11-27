ef8a6c7d9062d9aa6bdd87c6a0c65cdcb79f4f51
wada <115588841+wadagz@users.noreply.github.com>
2025-11-27 17:56:46 -0600

    feat: add migrations.
    Add migrations for creating `companies` and `tasks` tables.

---
+++ database/migrations/2025_11_27_231912_create_tasks_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('company_id')->constrained('companies');
+            $table->boolean('is_completed');
+            $table->timestamp('start_at');
+            $table->timestamp('expired_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
