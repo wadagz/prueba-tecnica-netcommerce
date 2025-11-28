385ce93b3ce2cb6ffef178118f680685c8ce4ca7
wada <115588841+wadagz@users.noreply.github.com>
2025-11-27 19:54:12 -0600

    fix: add mising properties and relation in model Task.
    Add mising property `hidden` and fillable fields.
    Add missing relation `user` and fixes name of relation `company`.

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -15,17 +15,41 @@ class Task extends Model
     public $fillable = [
         'name',
         'description',
+        'user_id',
+        'company_id',
         'is_completed',
         'start_at',
         'expired_at',
     ];
 
     /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'user_id',
+        'company_id',
+        'created_at',
+        'updated_at',
+    ];
+
+    /**
+     * Relation BelongsTo with Model User.
+     *
+     * @return BelongsTo<User, $this>
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
      * Relation BelongsTo with Model Company.
      *
      * @return BelongsTo<Company, $this>
      */
-    public function companies(): BelongsTo
+    public function company(): BelongsTo
     {
         return $this->belongsTo(Company::class);
     }
